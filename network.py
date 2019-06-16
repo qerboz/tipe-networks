4fbfcb715f240e01a7d9069eaae26d33dc27a276
qerboz <qerboz@users.noreply.github.com>
2019-06-17 01:37:39 +0200

    Update network.py

--- network.py
+++ network.py
@@ -13,38 +13,20 @@ import glob #gestion de fichier
 #Fonctions
 ##########
 
-###Générales
+###Generales
 def enleverDoublons(liste):
-    ''' Entrée: Une liste
+    ''' Entree: Une liste
         Sortie: La liste sans les doublons
-        note: Préserve l'ordre
+        note: Preserve l'ordre
         '''
-    res = []
+    sortie = []
     for x in liste:
         if x not in res:
-            res.append(x)
-    return res
-
-###Réseau de neurones  
-def elu(x):
-    """bijection de R dans ]-1,+inf[, continue dérivable strictement croissante"""
-    if x <0:
-       return np.exp(x)-1
-    return x
-  
-def dElu(x):
-    """dérivée de la fonction elu"""
-    if x<0:
-        return np.exp(x)
-    return 1
-
-def invElu(x):
-    if x<0:
-       return np.log(x+1)
-    return x
+            sortie.append(x)
+    return sortie
 
 def produitListes(L1,L2):
-    """entrer deux listes de taille n pour obtenir une liste de taille n dont chaque terme est le produit des termes de meme rang des listes d'entrée"""
+    """produit scalaire de deux listes"""
     L = []
     for i in range(len(L1)):
         L.append(L1[i]*L2[i])
@@ -61,29 +43,40 @@ def sommeListe(L):
         somme += x
     return somme
 
-def reverse(L):
+def retourner(L):
     return L[::-1]
 
-def train(reseau,base,nbr):
-    n = len(data)
-    for i in range(nbr):
-        foodIndex = randint(0,n-1)
-        reseau.compute( data[foodIndex][0] )
-        reseau.grad(sorties[data[foodIndex][1] ])
-        reseau.modifPoids()
-        reseau.modifBiais()
-        
+def normaliser(L):
+    return [np.floor(X/max(L)) for X in L]
+
+###Reseau de neurones  
+def elu(x):
+    """bijection de R dans ]-1,+inf[, continue, derivable strictement croissante"""
+    if x <0:
+       return np.exp(x)-1
+    return x
+  
+def dElu(x):
+    """derivee de la fonction elu"""
+    if x<0:
+        return np.exp(x)
+    return 1
+
+def invElu(x):
+    if x<0:
+       return np.log(x+1)
+    return x
 
 ###Traitement d'images
 def pixelValue(pix):
-    '''Entrée: Un tableau de trois valeurs représentant un pixel
-        Sortie: Ces trois valeurs concaténées.
+    '''Entree: Un tableau de trois valeurs representant un pixel
+        Sortie: Ces trois valeurs concatenees.
     '''
     return ('00'+str(pix[0]))[-3::] + ('00'+str(pix[1]))[-3::] + ('00'+str(pix[2]))[-3::]
 
 def imgEnNombres(image):
-    '''Entrée: Une image de taille nxp sous la forme d'un array numpy contenant les valeurs RGB de chaque pixel
-        Sortie: Une liste de même taille que le nombre de pixels de l'image contenant des valeurs représentant les pixels''' 
+    '''Entree: Une image de taille nxp sous la forme d'un array numpy contenant les valeurs RGB de chaque pixel
+        Sortie: Une liste de même taille que le nombre de pixels de l'image contenant des valeurs representant les pixels''' 
     n= len(image)
     p= len(image[0])
     return [int(pixelValue(image[i//p][i%p])) for i in range(n*p)]
@@ -92,101 +85,88 @@ def imgEnNombres(image):
 #Classes
 ########
 
-
-class layer():
-    """entrer le nombre de neurones sur cette couche et sur la couche précédente"""
+class couche():
+    """entrer le nombre de neurones sur cette couche et sur la couche precedente"""
     def __init__(self,n,p):
-        self.neurons = [random() for i in range(n)]
-        self.coefs = [[random() for i in range(p)] for j in range(n)]
-        self.biases = [random() for i in range(n)]
+        self.neurones = [random() for i in range(n)]
+        self.poids = [[random() for i in range(p)] for j in range(n)]
+        self.biais = [random() for i in range(n)]
 
-class neuralNetwork():
-    """entrer une liste comportant autant de termes qu'il y a de couches dans le réseau, et dont chaque terme correspond au nombre de neurones sur la couche associée à ce terme"""
+class reseauNeuronal():
+    """entrer une liste comportant autant de termes qu'il y a de couches dans le reseau, et dont chaque terme correspond au nombre de neurones sur la couche associee à ce terme"""
     def __init__(self,L):
-        self.layers = [layer(L[0],0)]#liste contenant les couches (des objets)
+        self.couches = [couche(L[0],0)]#liste contenant les couches (des objets)
         for i in range(1,len(L)): #Ne pas deborder dans la ligne suivante
-            self.layers.append(layer(L[i],L[i-1])) #création de chaque couche du réseau
+            self.couches.append(couche(L[i],L[i-1])) #creation de chaque couche du reseau
  
-    def compute(self,X): #calcule la sortie en fonction de l'entrée
+    def calcul(self,X): #calcule la sortie en fonction de l'entree
         for j in range(len(X)):
-            self.layers[0].neurons[j] = elu(X[j])
-        self.forward(0,len(self.layers))
-        return self.layers[-1].neurons
+            self.couches[0].neurones[j] = elu(X[j])
+        self.transfert(0,len(self.couches))
+        return self.couches[-1].neurones
  
-    def forward(self,i,n): #transfert des données des neurones d'une couche i vers la couche suivante
-        if i >= n-1: #arrête la récursivité
+    def transfert(self,i,n): #transfert des donnees des neurones d'une couche i vers la couche suivante
+        if i >= n-1: #arrête la recursivite
             return
-        for k in range(len(self.layers[i+1].neurons)):
-            valeur =  elu(sommeListe(produitListes(self.layers[i].neurons,self.layers[i+1].coefs[k]))+self.layers[i+1].biases[k])
-            self.layers[i+1].neurons[k] = valeur
-        self.forward(i+1,n)#récursivité pour transférer les données de la première couche à la dernière
+        for k in range(len(self.couches[i+1].neurones)):
+            valeur =  elu(sommeListe(produitListes(self.couches[i].neurones,self.couches[i+1].poids[k]))+self.couches[i+1].biais[k])
+            self.couches[i+1].neurones[k] = valeur
+        self.transfert(i+1,n)#recursivite pour transferer les donnees de la premiere couche à la derniere
  
-    def cost(self,X,Y):
-        self.cost = 0
-        compute(self,X)
-        for i in range(len(self.layers[-1].neurons)):
-            self.cost += (self.layers[-1].neurons[i]-elu(Y[i]))**2
+    def cout(self,X,Y):
+        self.cout = 0
+        calcul(self,X)
+        for i in range(len(self.couches[-1].neurones)):
+            self.cout += (self.couches[-1].neurones[i]-elu(Y[i]))**2
 
-    def grad(self,theor):
-        Y = self.layers[-1].neurons #Couche de sortie/reponse
-        self.ecart = [[dElu(invElu(self.layers[-1].neurons[i]))*(Y[i] - theor[i]) for i in range(len(Y))]]#La liste accueillant ecarts du coût par rapport à chaque poids
-        for k in range(len(self.layers)-1): #Pour chaque couche du reseau moins la derniere (deja effectue)
-            self.ecart.append([dElu(invElu(self.layers[-1-k-1].neurons[i]))*sommeListe(produitListes([X[i] for X in self.layers[-1-k].coefs],self.ecart[-1])) for i in range(len(self.layers[-1-k-1].neurons))])
-        self.ecart = reverse(self.ecart)
+    def calcErreur(self,theor):
+        Y = self.couches[-1].neurones #Couche de sortie/reponse
+        self.erreur = [[dElu(invElu(self.couches[-1].neurones[i]))*(Y[i] - theor[i]) for i in range(len(Y))]]#La liste accueillant erreurs du cout par rapport à chaque poids de la derniere couche
+        for k in range(len(self.couches)-1): #Pour chaque couche du reseau moins la derniere (deja effectuee)
+            self.erreur.append([dElu(invElu(self.couches[-1-k-1].neurones[i]))*sommeListe(produitListes([X[i] for X in self.couches[-1-k].poids],self.erreur[-1])) for i in range(len(self.couches[-1-k-1].neurones))])
+        self.erreur = retourner(self.erreur)
                 
     def modifPoids(self):
-        for k in range(1,len(self.layers)):
-            for i in range(len(self.layers[k].coefs)):
-                for j in range(len(self.layers[k].coefs[i])):
-                    self.layers[k].coefs[i][j] -= self.ecart[k][i]*0.05*self.layers[k-1].neurons[j]
+        for k in range(1,len(self.couches)):
+            for i in range(len(self.couches[k].poids)):
+                for j in range(len(self.couches[k].poids[i])):
+                    self.couches[k].poids[i][j] -= self.erreur[k][i]*0.05*self.couches[k-1].neurones[j]
 
     def modifBiais(self):
-        for i in range(1,len(self.layers)):
-            for j in range(len(self.layers[i].neurons)):
-                self.layers[i].biases[j] -= self.ecart[i][j]*0.05
+        for i in range(1,len(self.couches)):
+            for j in range(len(self.couches[i].neurones)):
+                self.couches[i].biais[j] -= self.erreur[i][j]*0.05
 
-    def train(self,nbr,v):
-        n = len(self.layers[0].neurons)
-        p = len(self.layers[-1].neurons)
-        result = [[random() for i in range(p)] for i in range(v)]
-        food = [[random() for i in range(n)] for i in range(v)]
-        for k in range(nbr):
-            a = randint(0,v-1)
-            self.compute(food[a])
-            self.grad(result[a])
-            self.modifPoids()
-            self.modifBiais()
-        for i in range(v):
-            print(food[i] , 'doit renvoyer' , result[i])
+    def entrainer(reseau,base,nbr):
+        n = len(base)
+        for i in range(nbr):
+            entree = randint(0,n-1)
+            reseau.calcul(base[entree][0] )
+            reseau.calcErreur(sorties[base[entree][1]])
+            reseau.modifPoids()
+            reseau.modifBiais()
+
 
 #####
 #Main
 #####
-reconnaisseur = neuralNetwork([25,20,17,10,6])
+reconnaisseur = reseauNeuronal([25,20,15,10,6])
 
 ###obtention des donnees d'entrainement
-os.chdir(".\TrainDB")
-trainData_name = glob.glob("*.png")
-especes = enleverDoublons([name.split("_")[0] for name in trainData_name])
+os.chdir("..\TrainDB")
+donneesEntrainementNoms = glob.glob("*.png")
+especes = enleverDoublons([name.split("_")[0] for name in donneesEntrainementNoms])
 
-trainData = np.array([ np.floor(img.imread(trainData_name[i]) * 255).astype(np.uint8) for i in range(len(trainData_name))])
-trainData = [( imgEnNombres(trainData[i]), trainData_name[i].split("_")[0] ) for i in range(len(trainData))]
+donneesEntrainement = np.array([np.floor(img.imread(donneesEntrainementNoms[i])*255).astype(np.uint8) for i in range(len(donneesEntrainementNoms))])
+donneesEntrainement = [(imgEnNombres(donneesEntrainement[i]),donneesEntrainementNoms[i].split("_")[0]) for i in range(len(donneesEntrainement))]
 
 
 ###obtention des donnees de test
 os.chdir("..\TestDB")
-testData_name = glob.glob("*.png")
-testData = np.array([ np.floor(img.imread(trainData_name[i]) * 255).astype(np.uint8) for i in range(len(trainData_name))])
-testData = [( imgEnNombres(trainData[i]), trainData_name[i].split("_")[0] ) for i in range(len(trainData))]
+donneesEntrainementNoms = glob.glob("*.png")
+donneesEntrainement = np.array([np.floor(img.imread(donneesEntrainementNoms[i])*255).astype(np.uint8) for i in range(len(donneesEntrainementNoms))])
+donneesEntrainement = [(imgEnNombres(donneesEntrainement[i]),donneesEntrainementNoms[i].split("_")[0] ) for i in range(len(donneesEntrainement))]
 
-###Sorties théoriques à partir du nom de l'espèce
-sorties = [ ( especes[i] , [ 1*(i==j) for j in range(len(especes))] ) for i in range(len(especes))]
+###Sorties theoriques à partir du nom de l'espèce
+sorties = [(especes[i],[1*(i==j) for j in range(len(especes))]) for i in range(len(especes))]
 sorties = dict(sorties)
-
-
-
-
-
-
-
-
